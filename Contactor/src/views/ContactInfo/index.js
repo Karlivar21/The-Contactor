f3f801853cf366e4b3954617473816923737d81f
HaukurS <89972635+HaukurS@users.noreply.github.com>
2022-11-28 14:05:17 +0000

    'bla'

--- Contactor/src/views/ContactInfo/index.js
+++ Contactor/src/views/ContactInfo/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, TouchableHighlight, TextInput, Image } from 'react-native'
+import { View, Text, TouchableHighlight, Image } from 'react-native'
+import Toolbar from '../../components/toolbar'
 import styles from './styles'
 
 export default function ContactInfo ({ route }) {
@@ -9,14 +10,26 @@ export default function ContactInfo ({ route }) {
   const id = route.params.id
   console.log(image)
 
+//   const editContact = (contact) => {
+//         const lastId = contacts.length
+//         contact.id = lastId + 1
+//         contact.thumbnailPhoto = photo
+//         setContacts((currentContacts) => {
+//         return [...contacts, contact]
+//         })
+//         setOpenContact(false)
+//         setSelectedPhoto(false)
+//   }
+
+
   return (
         <View style={styles.container}>
+            <Toolbar
+            onEdit = {() => setOpendContact(true)}
+            />
             <View style={styles.contact}>
                 <Image style={styles.image} source={{ uri: image }}/>
                 <Text style={styles.name}>{name}</Text>
-                <TouchableHighlight>
-                    <Text style={styles.edit}>Edit</Text>
-                </TouchableHighlight>
             <View style={styles.number}>
                 <Text style={styles.phone}>+354 {phoneNumber}</Text>
                 </View>
